2764e02afa4828f11f5a46ceeb893c907b648c2c
jacksonchen1998 <present90308@gmail.com>
2023-06-12 15:10:54 +0800

    [DEL]

--- bert_test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-a=np.array([[1,2,3],[4,5,6],[7,8,9]])
-b=[[0,1,2],[2,1,0]]
-print(a[b,b])
--- test.py
+++
@@ -1,3 +0,0 @@
-from transformers import GPT2Tokenizer
-gpt_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-print(gpt_tokenizer(' the'))
